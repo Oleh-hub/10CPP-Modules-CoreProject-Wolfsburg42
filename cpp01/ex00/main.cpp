61d00262db6c20b722d7f5d4da6a56da6844e324
Oleh Ruban <oruban@c2r9s2.42wolfsburg.de>
2024-08-29 19:28:18 +0200

    starting cpp01/ex01/

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,14 +6,14 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:21:58 by oruban            #+#    #+#             */
-/*   Updated: 2024/08/29 14:25:24 by oruban           ###   ########.fr       */
+/*   Updated: 2024/08/29 14:30:21 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main(void) {
-	
+//============ Creation on stack	
 	
 	std::string consol_name;
 	std::cout << "Enter name of the zombie that will be created on stack: " << std::flush;
@@ -21,7 +21,7 @@ int main(void) {
 	
 	Zombie stack_zombie(consol_name);
 	stack_zombie.announce();
-//=================
+//================= Creation on heap via newZombie()
 	std::string heapzombie_name;
 	std::cout << "Enter name of the zombie that will be created on heap: " << std::flush;
 	std::cin >> heapzombie_name;
@@ -29,8 +29,7 @@ int main(void) {
 	Zombie *heap_zombie;
 	heap_zombie = newZombie(heapzombie_name);
 	heap_zombie->announce();
-	
-//=================
+//================= Creation on stack via randomChump()
 	std::string randomchampzombie_name;
 	std::cout << "Enter name of zombie for randomChump() - creation on stack: " << std::flush;
 	std::cin >> randomchampzombie_name;
