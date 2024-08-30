ac2dea03ce23b2b29b70ee1140ef2161ab5e937b
Oleh Ruban <oruban@c2r9s4.42wolfsburg.de>
2024-08-30 12:51:50 +0200

    cpp02/ex03 Weapon started

---
+++ cpp01/ex03/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 12:48:40 by oruban            #+#    #+#             */
+/*   Updated: 2024/08/30 12:49:19 by oruban           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main() {
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
