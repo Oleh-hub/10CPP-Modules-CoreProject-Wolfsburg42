f4da5554388a8bf4831d9bdb193f7d44be455931
Oleh Ruban <oruban@c2r9s4.42wolfsburg.de>
2024-08-30 13:02:36 +0200

    cpp01/ex01 brain is expanded to fulfill the subj

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 12:41:45 by oruban            #+#    #+#             */
-/*   Updated: 2024/08/30 12:42:38 by oruban           ###   ########.fr       */
+/*   Updated: 2024/08/30 13:01:13 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,12 @@ int main() {
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
+	std::cout << "The addresses of: str, stringPTR and stringREF." << std::endl;
 	std::cout << "The memory address of the string variable str:	" << &str << std::endl;
-	std::cout << "The value pointed to by stringPTR:		" << stringPTR << std::endl;
-	std::cout << "The value pointed to by stringREF:		" << &stringREF << std::endl;
+	std::cout << "The memory address held by stringPTR:		" << stringPTR << std::endl;
+	std::cout << "• The memory address held by stringREF:		" << &stringREF << std::endl;
+	std::cout << std::endl << "The values of: str, stringPTR and stringREF." << std::endl;
+	std::cout << "The value of the string variable str:	" << str << std::endl;
+	std::cout << "The value pointed to by stringPTR:	" << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF:	" << stringREF << std::endl;
 }
