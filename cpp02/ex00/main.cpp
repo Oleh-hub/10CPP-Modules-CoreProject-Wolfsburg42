fd13766677306a1bc68ff1076dc1d5667cb53abb
Oleh Ruban <oruban@c2r9s2.42wolfsburg.de>
2024-09-18 11:23:10 +0200

    in copy assignment operator overload the check against self assignment added

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 20:34:02 by oruban            #+#    #+#             */
-/*   Updated: 2024/09/17 20:35:18 by oruban           ###   ########.fr       */
+/*   Updated: 2024/09/18 11:00:15 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,25 @@ int main( void )
 	Fixed b( a );
 	Fixed c;
 
-	c = b;
+	c.operator= (b);
+	// c = b;
 	
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 	return 0;
 }
+
+/* int main( void ) 
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+} */
