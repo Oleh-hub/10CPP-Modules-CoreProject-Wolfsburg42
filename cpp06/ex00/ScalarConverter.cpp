2ed513abda23233f00c269154ded0436d7093623
Oleh Ruban <polihronii@gmail.com>
2025-01-16 21:06:01 +0100

    ScalarConverter::getType() under construction

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,9 +1,9 @@
-
 #include "ScalarConverter.hpp"
+#include <vector>
 
-ScalarConverter::~ScalarConverter()
-{
-}
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
 
 ScalarConverter & ScalarConverter::operator=(ScalarConverter &rhs)
 {
@@ -11,7 +11,58 @@ ScalarConverter & ScalarConverter::operator=(ScalarConverter &rhs)
 	return *this;
 }
 
-void ScalarConverter::convert(const std::string &rhs)
+ScalarConverter::eScalarType ScalarConverter::getType(const std::string &str)
 {
-	std::cout << rhs << std::endl;
+	//Удобство извлечения данных(>>), Обработка ошибок, Пошаговый анализ строки
+	std::istringstream iss(str); //  "input string stream" (поток ввода из строки) - makes the work with string easier
+	int b4Dot;					// number from iss b4 dot '.'
+	char dot;
+
+	std::cout << RED "DEBUG getType(): str = "  << str << "; length() = " << str.length() << RESET << std::endl;
+	if (str.length() == 1 && !std::isdigit(str[0]))
+		return CHAR;
+	
+	if (iss >> b4Dot)
+	{
+		std::cout << BLUE << b4Dot << RESET << std::endl;
+		if (iss.eof())
+			return INT;
+
+		// if ()
+
+	}
+	return INVALID;
+}
+
+void ScalarConverter::convert(const std::string &str)
+{
+	std::cout << "String representation of a C++ literal: " << str << std::endl;
+	eScalarType scalarType = getType(str);
+	(void) scalarType;
+
+	// eScalarType scalarType1[] = {CHAR, INT, FLOAT, DOUBLE, INVALID};
+	for (int i = 0; i < 5; i++)
+	{
+		// eScalarType scalarType = type[i];
+		{
+			switch (scalarType)
+			{
+				case (CHAR):
+					std::cout << scalarType << std::endl;
+					break;
+				case (INT):
+					std::cout << scalarType << std::endl;
+					break;
+				case (FLOAT):
+					std::cout << scalarType << std::endl;
+					break;
+				case (DOUBLE):
+					std::cout << scalarType << std::endl;
+					break;
+				case (INVALID):
+					std::cout << scalarType << std::endl;
+					break;
+			}
+		}
+	}
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -2,16 +2,30 @@
 # define SCALARCONVERTER_HPP
 # include <iostream>
 # include <string>
+# include <sstream>
+# include "Colors.hpp"
 
 class ScalarConverter
 {
 	private:
-		ScalarConverter();
 		ScalarConverter(const ScalarConverter & other);
 		ScalarConverter &operator=(ScalarConverter &rhs);
+		
+		enum eScalarType
+		{
+			CHAR,
+			INT,
+			FLOAT,
+			DOUBLE,
+			INVALID
+		};
+		ScalarConverter();
 		~ScalarConverter();
+
+
 	public:
 		static void convert(const std::string &rhs);
+		static eScalarType getType(const std::string &rhs);
 };
 
 #endif // SCALARCONVERTER_HPP
